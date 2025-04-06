2bc6d4e0c0637b1c5d6710f08cd1af78845af78a
Chon <chon_na_krup@hotmail.com>
2025-04-07 05:03:32 +0700

    Basic Syntax Parallel Loops

---
+++ ParellelLoops/BasicSyntax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParellelLoops
+{
+    public static class BasicSyntax
+    {
+        public static void Run()
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            int sum = 0;
+            object lockObject = new object();
+
+            //Parallel.For(0, array.Length, i =>
+            //{
+            //    // This is a thread-safe operation
+            //    // The lock statement is used to ensure that only one thread can access the shared resource at a time
+            //    lock (lockObject)
+            //    {
+            //        sum += array[i];
+            //    }
+
+            //});
+
+            Parallel.ForEach(array, i =>
+            {
+                // This is a thread-safe operation
+                // The lock statement is used to ensure that only one thread can access the shared resource at a time
+                lock (lockObject)
+                {
+                    sum += i;
+                }
+            });
+
+            Console.WriteLine($"The Sum is {sum}");
+            Console.ReadLine();
+
+            // Parallel.Invoke
+            Parallel.Invoke(
+                () => Console.WriteLine("Task 1"),
+                () => Console.WriteLine("Task 2"),
+                () => Console.WriteLine("Task 3")
+            );
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ParellelLoops/Program.cs
@@ -0,0 +1,6 @@
+﻿// Parellel loops helps us to devide or conquer
+// Parallel loops helps us to write multi-threading programming just write sequential programming
+
+using ParellelLoops;
+
+BasicSyntax.Run();
